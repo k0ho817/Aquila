7958b18df45b83055342d78bb0533b93722a164b
k0ho817 <mkh990817@gmail.com>
2025-07-04 06:14:07 +0000

    [EDIT] train.py | 3090 *2 환경에 맞게 학습 파라미터 조정

--- vision/train.py
+++ vision/train.py
@@ -5,9 +5,11 @@ model = YOLO('yolov8n.pt')
 model.train(
     data='/home/mkh99/Aquila/data/Aquila-1/data.yaml',
     epochs=100,
-    imgsz=640,
-    batch=160,
-    name='aquila_v8n_epoch100_pt10',
+    imgsz=960,
+    batch=128,
+    name='aquila_v8n_epoch100_2gpu_3090',
     patience=10,
-    device='0,1'
+    device='0,1',
+    workers=8,
+    amp=True
 )
